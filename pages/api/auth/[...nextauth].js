6f565eee3ca3efe2d00f68326ea4b39b47392f29
anshu0x <anshusharma6327@gmail.com>
2022-11-17 23:20:55 +0530

    github auth added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 export const authOptions = {
     // Configure one or more authentication providers
     providers: [
@@ -7,6 +8,10 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+          })
         // ...add more providers here
     ],
     secret: process.env.JWT_SECRET,
