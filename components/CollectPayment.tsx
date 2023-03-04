b7b56bb405fbcb628e24a1c383eee5bb2112e643
anshu0x <anshusharma6327@gmail.com>
2023-03-04 21:24:28 +0530

    qr code saver added

--- components/CollectPayment.tsx
+++ components/CollectPayment.tsx
@@ -2,93 +2,93 @@ import Image from 'next/image'
 const CollectPayment = () => {
   return (
     <section className="text-gray-600 body-font text-center">
-        <h1 className="font-bold text-4xl text-black leading-10 my-4">
-          Collect Payments in 3 Easy Steps
-        </h1>
-        <div className="container mx-auto gap-16 sm:gap-32 flex px-10 py-8 md:flex-row flex-col items-center">
-          <div className="lg:max-w-lg lg:w-full md:w-1/2">
-            <Image alt="hero" src={'/collectPayments.webp'} width={"400"} height="500" />
+      <h1 className="font-bold text-4xl text-black leading-10 my-4">
+        Collect Payments in 3 Easy Steps
+      </h1>
+      <div className="container mx-auto gap-16 sm:gap-32 flex px-10 py-8 md:flex-row flex-col items-center">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2">
+          <Image alt="hero" src={'/collectPayments.webp'} width={"400"} height="500" />
+        </div>
+        <div className="lg:flex-grow flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <div className="flex relative pb-12">
+            <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+            </div>
+            <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-4">
+              <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                STEP 1
+              </h2>
+              <p className="leading-relaxed">
+                Create Payment Link Via Upi Pay Dashboard
+              </p>
+            </div>
           </div>
-          <div className="lg:flex-grow flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 1
-                </h2>
-                <p className="leading-relaxed">
-                  Create Payment Link Via Upi Pay Dashboard
-                </p>
-              </div>
+          <div className="flex relative pb-12">
+            <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+              <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
             </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
-              </div>
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 2
-                </h2>
-                <p className="leading-relaxed">
-                  Share Payment Link Through SMS, Email or WhatsApp
-                </p>
-              </div>
+            <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
             </div>
-            <div className="flex relative pb-12">
-              <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <circle cx="12" cy="5" r="3"></circle>
-                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-                </svg>
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
-                  STEP 3
-                </h2>
-                <p className="leading-relaxed">
-                  Customer Makes Payment Using Preferred Payment Method
-                </p>
-              </div>
+            <div className="flex-grow pl-4">
+              <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                STEP 2
+              </h2>
+              <p className="leading-relaxed">
+                Share Payment Link Through SMS, Email or WhatsApp
+              </p>
+            </div>
+          </div>
+          <div className="flex relative pb-12">
+            <div className="flex-shrink-0 w-10 h-10 rounded-full bg-[#0295fc] inline-flex items-center justify-center text-white relative">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="12" cy="5" r="3"></circle>
+                <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-4">
+              <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">
+                STEP 3
+              </h2>
+              <p className="leading-relaxed">
+                Customer Makes Payment Using Preferred Payment Method
+              </p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
--- pages/pay/[id].tsx
+++ pages/pay/[id].tsx
@@ -1,47 +1,72 @@
-import React from "react";
+import React, { useRef } from "react";
 import QRCode from "react-qr-code";
 import HeaderLogo from "../../components/HeaderLogo";
 import { GetServerSideProps } from "next";
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
+import toast from "react-hot-toast";
+import axios from "axios";
 const Pay = ({ link }: { link: Link }) => {
   const { upiId, amount, name, description } = link;
   let upiLink = `upi://pay?pa=${upiId}&pn=${name}&am=${amount}&cu=INR&tn=${description}`;
+  const captureRef = useRef<HTMLDivElement | null>(null);
+  const handleCapture = async () => {
+    const toastId = toast.loading('Downloading...');
+    try {
+      const canvas = await html2canvas(captureRef.current)
+      canvas.toBlob((blob) => {
+        saveAs(blob, "image.png");
+        toast.success('Successfully saved ', {
+          id: toastId,
+        });
+      });
+
+    } catch (error) {
+      toast.error("An error occured!", {
+        id: toastId,
+      });
+    }
+  };
   return (
     <>
       <div className="flex flex-row mt-6  sm:mt-0 justify-center">
-        <div className="relative flex-col flex gap-4 sm:w-fit rounded-lg p-8 ">
-          <div className="grid sm:my-0  my-4">
-            <HeaderLogo />
+        <div >
+          <div ref={captureRef} className="relative flex-col flex gap-4 sm:w-fit rounded-lg p-8 ">
+            <div className="grid sm:my-0 my-4 grid-cols-1">
+              <HeaderLogo />
+            </div>
+            <div className="flex justify-center">
+              <QRCode value={upiLink} size={180} />
+            </div>
+            <div className="flex gap-2 flex-col text-left px-8">
+              <div>
+                <h3 className="text-xl font-bold text-gray-900">Pay to</h3>
+                <p className="text-sm font-medium text-gray-600">{name}</p>
+              </div>
+              <div>
+                <h3 className="text-xl font-bold text-gray-900">Upi Id</h3>
+                <p className="text-sm font-medium text-gray-600">{upiId}</p>
+              </div>
+              <div className="flex flex-col-reverse">
+                <p className="text-xs font-bold text-gray-500">₹ {amount}</p>
+                <p className="text-xl font-bold text-gray-900">Amount Payable</p>
+              </div>
+              <div>
+                <p className="text-xl font-bold text-gray-900">
+                  Purpose of payment
+                </p>
+                <p className="text-sm text-gray-500">{description}</p>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center">
-            <QRCode value={upiLink} size={180} />
-          </div>
-          <div className="flex gap-2 flex-col text-left px-8">
-            <div>
-              <h3 className="text-xl font-bold text-gray-900">Pay to</h3>
-              <p className="text-sm font-medium text-gray-600">{name}</p>
-            </div>
-            <div>
-              <h3 className="text-xl font-bold text-gray-900">Upi Id</h3>
-              <p className="text-sm font-medium text-gray-600">{upiId}</p>
-            </div>
-            <div className="flex flex-col-reverse">
-              <p className="text-xs font-bold text-gray-500">₹ {amount}</p>
-              <p className="text-xl font-bold text-gray-900">Amount Payable</p>
-            </div>
-            <div>
-              <p className="text-xl font-bold text-gray-900">
-                Purpose of payment
-              </p>
-              <p className="text-sm text-gray-500">{description}</p>
-            </div>
-            <div className="mt-6 flex justify-center">
-              <a
-                className="inline-block rounded border bg-[#002970] w-36 text-center py-3 text-sm font-bold text-white"
-                href={upiLink}
-              >
-                Pay ⚡
-              </a>
-            </div>
+          <div className="mt-6 grid justify-center">
+            <a
+              className="grid place-content-center rounded border bg-[#002970] w-full text-center py-3 text-sm font-bold text-white"
+              href={upiLink}
+            >
+              Pay ⚡
+            </a>
+            <button className="grid place-content-center px-4 rounded border bg-[#002970] w-full text-center py-3 text-sm font-bold text-white" onClick={handleCapture}>Download as Image</button>
           </div>
         </div>
       </div>
@@ -52,11 +77,21 @@ const Pay = ({ link }: { link: Link }) => {
 export default Pay;
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const res = await fetch(
-    `https://anshu.up.railway.app/genlink/uid/${params.id}`
-  );
-  const [link] = await res.json();
-  return {
-    props: { link },
-  };
+  try {
+    const { data } = await axios<[Link]>(
+      `https://anshu.up.railway.app/genlink/uid/${params.id}`
+    );
+    const [link] = data
+    if (data.length) {
+      return {
+        props: { link },
+      };
+    }
+    return {
+      notFound: true,
+      redirect: "/"
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
